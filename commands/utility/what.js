8f85d6724d6722e676d0f97d00d1370c689b4bba
DJ Neill <dj.neill856@gmail.com>
2024-04-24 04:10:42 -0500

    add slash command

---
+++ commands/utility/what.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+const link = 'https://leonnoel.com/100devs/'
+
+module.exports = {
+    cooldown: 5,
+    data: new SlashCommandBuilder()
+        .setName('wha_is-100Devs')
+        .setDescription('Provides information about 100Devs.'),
+    async execute(interaction) {
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        await interaction.reply(`${interaction.user.globalName} wants to learn more about 100Devs, this is the information you're looking for. ${link}`);
+    },
+};
+
